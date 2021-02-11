7f1d38239ae8ea62d8a172771eeb05f4ab8cf41e
Ale Or <aocaraballo19@iespuntadelverde.es>
2021-02-11 18:33:25 +0100

    Menú básico hecho

---
+++ main_page.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main_page_styles.css">
+    <title>Document</title>
+</head>
+<body>
+	<div class="menu-container">
+		<input type="checkbox" id="menuicon" class="hamburger-checkbox">
+		<div class="hamburger-icon">
+    		<label for="openmenu" id="hamburger-label">
+      			<span id="span1"></span>
+      			<span id="span2"></span>
+      			<span id="span3"></span>
+      			<span id="span4"></span>
+    		</label>    
+  		</div>
+		<div class="menu-pane">
+      
+      	<nav>
+        	<ul class="menu-links">
+				<li><a href="###">Québec, QC</a>
+					<span id="QC-info">
+						<p>+1 418-640-3300</p>
+					</span>
+				</li>
+			
+				<li>
+					<a href="###">déjeuner</a>
+				</li>
+
+				<li>
+					<a href="###">boulangerie</a>
+				</li>
+				<li>
+					<a href="###">boissons</a>
+				</li>
+        	</ul>
+         	<ul class="menu-links">
+                <li>
+					<a href="###">Washington, DC</a>
+                    <span id="DC-info">
+            			<p>+1 202-472-3850</p>
+          			</span>
+				</li>
+          		<li>
+					<a href="###">breakfast</a>
+				</li>
+          		<li>
+					<a href="###">bakery</a>
+				</li>
+          		<li>
+					<a href="###">bar</a>
+				</li>
+        	</ul>
+      	</nav>
+    </div>
+	</div>
+</body>
+</html>
